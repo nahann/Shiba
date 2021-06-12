f5868cc380836280470feadc4d03ab509107abba
nahan <nathan.demichele@gmail.com>
2021-06-12 04:45:45 -0500

    added pull cmd

---
+++ commands/Developer/Pull.js
@@ -0,0 +1,18 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const { exec } = require('child_process')
+
+module.exports = {
+    name: 'force-pull',
+    description: `Pulles from the github repo.`,
+    ownerOnly: true,
+    guildOnly: true,
+    category: `Developer`,
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+      exec('git pull')
+    }
+}
