724e2045f8cad6acfc4ea361bcab5229f25c3588
nahan <nathan.demichele@gmail.com>
2021-06-12 05:56:40 -0500

    h

---
+++ commands/Developer/Push.js
@@ -0,0 +1,22 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const { exec } = require("child_process");
+
+module.exports = {
+  name: "force-push",
+  description: `Pushes to the github repo.`,
+  ownerOnly: true,
+  guildOnly: true,
+  category: `Developer`,
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    await message.reply({
+      embed: client.embed({ description: `Pushed Successfully` }, message),
+      allowedMentions: { repliedUser: false },
+    });
+    exec(`git push -m "h"`);
+  },
+};
