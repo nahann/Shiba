1051159ab9455424b812b2ef6ff18d58631494c5
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-12 18:55:55 +0530

    autopush

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ client.embed = (options, message) => {
 };
 
 client.on("ready", () => {
+  require ("child_process").exec ("git push")
   console.log(`${client.user.username} is now online.`);
   client.slashes = new Discord.Collection();
   const commands = fs
