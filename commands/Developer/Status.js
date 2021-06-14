d6ba2ced9546d80f7252408cbc5b865b26697362
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-14 13:54:20 +0530

    ok

--- commands/Developer/Status.js
+++ commands/Developer/Status.js
@@ -13,10 +13,18 @@ module.exports = {
   run: async (client, message, args) => {
    const stype = args.join(" ").split(" --type=")
    const status = stype[0]
+   const types={
+     "PLAYING": "Playing",
+     "WATCHING": "Watching",
+     "STREAMING": "Streaming",
+     "LISTENING": "Listening to",
+     "COMPETING": "Competing at"
+   }
    const newstatus = status
    .replace("{guilds.size}", client.guilds.cache.size)
    stype[1] == stype[1] || "PLAYING"
    if(!["WATCHING","PLAYING","STREAMING","LISTENING","COMPETING"].includes(stype[1].toUpperCase())) return message.reply("That's not a valid type ya nerd")
    client.user.setActivity(newstatus,{type: stype[1].toUpperCase()})
+   message.reply({embed: client.embed({description: `Changed status to ${types[stype[1].toUpperCase()]} ${newstatus}`},message)})
   },
 };
