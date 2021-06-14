a218f05fad9aa99e2d6a42e87e62aabe4278985c
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-14 10:58:35 +0530

    remove ready event

--- index.js
+++ index.js
@@ -75,32 +75,6 @@ client.embed = (options, message) => {
     )
     .setTimestamp();
 };
-
-client.on("ready", async() => {
-  require ("child_process").exec ("git push")
-  console.log(`${client.user.username} is now online.`);
-  client.slashes = new Discord.Collection();
-  const commands = fs
-    .readdirSync(`./commands-slash`)
-    .filter((comd) => comd.endsWith(".js"));
-  //Makes sure there are commands so it doesn't error
-  if (commands.length) {
-    commands.forEach((command) => {
-      const cmd = require(`./commands-slash/${command}`);
-
-      if (!cmd.name || !cmd.description || !cmd.run) return;
-
-      client.guilds.cache.get("849131192275566613").commands.create(cmd);
-      client.slashes.set(cmd.name, cmd);
-    });
-  }
-});
-
-client.on("interaction", (command) => {
-  if (!command.isCommand()) return;
-  client.slashes.get(command.commandName)?.run(client, command);
-});
-
 client.on("message", async (message) => {
   if (message.author.bot) return;
   const data = await GuildConfig.findOne({ guildId: message.guild.id });
