b33cdc0f3d8e8fe0bdb818b69e78371681ee4e27
nahan <nathan.demichele@gmail.com>
2021-06-14 01:21:20 -0500

    added slash cmd event

--- events/interaction.js
+++ events/interaction.js
@@ -1,6 +1,6 @@
 module.exports={
   name: "interaction",
-  run: async function(client, command){
+  run: async (command, client) => {
     if(!command.isCommand()) return;
     client.slashes.get(command.commandName)?.run(client,command)
   }
