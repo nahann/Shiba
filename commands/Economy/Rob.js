cbe1d570a5e116cfd24b459352c6b0abab589602
nahan <nathan.demichele@gmail.com>
2021-06-14 02:56:12 -0500

    cant rob poor people now 🏠❌

--- commands/Economy/Rob.js
+++ commands/Economy/Rob.js
@@ -18,11 +18,13 @@ module.exports = {
         if(message.author.id === user.id) return message.reply({ embed: client.embed({ description: `You cannot rob yourself.`}, message)})
         const authorData = await schema.findOne({ userId: message.author.id })
         const userData = await schema.findOne({ userId: user.id })
+        const userToken = userData.get('shibaToken')
         const authorPassive = authorData.get('Passive')
         const userPassive = userData.get('Passive')
+        if(userToken < 500)return message.reply({ embed: client.embed({ description: `The person you are trying to rob has less than 500 Shiba Token.`}, message)})
         if(authorPassive === true) return message.reply({ embed: client.embed({ description: `You cannot rob someone while you are on passive mode.`}, message)})
         if(userPassive === true) return message.reply({ embed: client.embed({ description: `The user you are trying to rob is on passive mode.`}, message)})
-        const token = Math.floor(Math.random() * 100);
+        const token = Math.floor(Math.random() * 350);
         schema.increment({ userId: message.author.id }, "shibaToken", token)
         schema.decrement({ userId: user.id }, "shibaToken", token)
         return message.reply({ embed: client.embed({ description: `${message.author.username} stole ${token} from ${user.username}`}, message)})
