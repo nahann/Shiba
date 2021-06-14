c4055f9f1fcacd6e26cf7c4b129b1d001e3c4f9f
nahan <nathan.demichele@gmail.com>
2021-06-14 00:29:39 -0500

    h

--- index.js
+++ index.js
@@ -117,24 +117,6 @@ client.on("message", async (message) => {
     return message.reply({ embed: client.embed({ description: `Try this command in ${ms(client.cooldowns.get(`${message.author.id}-${command.name}`) - Date.now(), { long: true })}`}, message)})
   }
 
-//   if (!cooldowns.has(command.name)) {
-// 	cooldowns.set(command.name, new Discord.Collection());
-//   }
-
-//   const now = Date.now();
-//   const timestamps = cooldowns.get(command.name);
-//   const cooldownAmount = (command.cooldown || 3) * 1000;
-
-//   if (timestamps.has(message.author.id)) {
-// 	const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-// 	if (now < expirationTime) {
-// 		const timeLeft = (expirationTime - now) / 1000;
-// 		return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-// 	}
-//  }
-// timestamps.set(message.author.id, now);
-// setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
 
   if (command.guildOnly && message.channel.type === "dm") {
