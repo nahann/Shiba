63c54318e554f9d703b2e2af0ccbfab7fa1cef51
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-14 09:20:02 +0530

    ok

--- index.js
+++ index.js
@@ -113,7 +113,28 @@ client.on("message", async (message) => {
     );
 
   if (!command) return;
+  if(command.cooldown){
+  const { cooldowns } = client;
 
+  if (!cooldowns.has(command.name)) {
+	cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+	const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+	if (now < expirationTime) {
+		const timeLeft = (expirationTime - now) / 1000;
+		return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+	}
+ }
+timestamps.set(message.author.id, now);
+setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+ }
   if (command.guildOnly && message.channel.type === "dm") {
     return message.channel.send(`This is a guild only command.`);
   }
