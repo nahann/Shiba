8deb0034666d114ba046a43e2b649c1aad7ebca3
owencubes <63976821+owencubes@users.noreply.github.com>
2021-06-14 13:51:11 -0400

    img-manipulation

---
+++ commands/Fun/magik.js
@@ -0,0 +1,17 @@
+const { get } = require("axios");
+
+module.exports = {
+  name: "magik",
+  description: "very distortion",
+  run: async (bot, message, args) => {
+    const avatar = message.mentions.users.first().displayAvatarURL({ dynamic: false }) || message.author.displayAvatarURL({ dynamic: false }) 
+    try {
+      const { data } = await get(
+        `https://nekobot.xyz/api/imagegen?type=magik&image=${avatar}&intensity=10`
+      );
+      message.reply(client.embed({ image: data.message }, message));
+    } catch (err) {
+      return console.error(err.message);
+    }
+  },
+};
