0b37673cdbbcce84ce00aae0db1d4c1fae9237f8
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-14 09:06:33 +0530

    ok

--- commands/Economy/Beg.js
+++ commands/Economy/Beg.js
@@ -1,9 +1,12 @@
 module.exports={
    name: "beg",
+   beta: true,
    description: "idk beg",
    run: async(client,message)=>{
      const schema = await client.db.load("userEcos")
-     const user = schema.findOne({ userId: message.author.id }) || schema.create({userId: message.author.id })
-     
+     const user = await schema.findOne({ userId: message.author.id }) || await schema.create({userId: message.author.id })
+     const tokens = Math.floor(Math.random() * 300)
+     schema.increment({userId: message.author.id},"shibaToken",tokens)
+     message.reply({embed: client.embed({description: `You begged and got ${tokens} shibaTokens`},message)})
    }
 }
