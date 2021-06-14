4619ed722db15d4813c9d4f58b8836a5d24c638c
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-14 14:25:49 +0530

    some fixing

--- commands/Info/covid.js
+++ commands/Info/covid.js
@@ -1,14 +1,12 @@
-//https://random-d.uk/api/v2/random
-const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 module.exports = {
   name: "covid",
   aliases: ["corona", "covid19", "cr", "coronavirus"],
   description: "returns the COVID-19 stats of a country",
-  run: async (bot, message, args) => {
-    if (!args.length) message.channel.send("You have to specify a country!");
+  run: async (client, message, args) => {
+    if (!args.length) message.reply("You have to specify a country!");
     fetch(`https://disease.sh/v3/covid-19/countries/${args.join(" ")}`)
-      .then((ducc) => ducc.json())
+      .then((info) => info.json())
       .then((json) => {
         if (json.country == "undefined") {
           message.channel.send(
@@ -16,14 +14,13 @@ module.exports = {
           );
         }
 
-        const e = new MessageEmbed()
+        const e = client.embed({},mesaage)
           .setTitle(`The covid-19 stats of ${json.country}`)
           .setThumbnail(json.countryInfo.flag)
           .setDescription(
             ` Cases today: \`\`\`${json.todayCases}\`\`\`\nDeaths today: \`\`\`${json.todayDeaths}\`\`\`\nAll cases: ${json.cases}\n All deaths: ${json.deaths}\n All recoveries: ${json.recovered}\n Active cases right now: ${json.active} `
           )
-          .setColor("RANDOM");
-        message.channel.send(e);
+        message.reply({embed: e});
       });
   },
 };
