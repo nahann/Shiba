548f5d6bd9db0cb39e9d3dee2f7c509f7cdc4fb9
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-14 14:52:32 +0530

    ok

--- commands/Info/covid.js
+++ commands/Info/covid.js
@@ -17,7 +17,7 @@ module.exports = {
         const e = client.embed({title: `COVID-19 stats of ${json.country}`},message)
           .setThumbnail(json.countryInfo.flag)
           .addField(
-            `Cases today`,`\`\`\`${json.todayCases}\`\`\``,true).addField(`Deaths today`,`\`\`\`${json.todayDeaths}\`\`\`,true).addField(`All cases`,`\`\`\`${json.cases}\`\`\``,true).addField(`All deaths`,`${json.deaths}`,true).addField(`All recoveries`,`\`\`\`${json.recovered}\`\`\``,true).addField(`Active cases right now`,`\`\`\`${json.active}\`\`\``,true)
+            `Cases today`,`\`\`\`${json.todayCases}\`\`\``,true).addField(`Deaths today`,`\`\`\`${json.todayDeaths}\`\`\``,true).addField(`All cases`,`\`\`\`${json.cases}\`\`\``,true).addField(`All deaths`,`${json.deaths}`,true).addField(`All recoveries`,`\`\`\`${json.recovered}\`\`\``,true).addField(`Active cases right now`,`\`\`\`${json.active}\`\`\``,true)
       
         message.reply({embed: e});
       });
