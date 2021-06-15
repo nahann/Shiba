76aa83da9be6022194ce3dcd412099ac55e97299
{Matthew} <68120990+MatthewDisc@users.noreply.github.com>
2021-06-15 06:58:43 +0000

    Update random.js

--- commands/Fun/random.js
+++ commands/Fun/random.js
@@ -4,6 +4,7 @@ module.exports = {
   description: "randomizes the given arguments",
   aliases: ["randomword", "rw"],
   run: async(client, message, args) => {
+    if (!args.length) return message.reply("you need some text there!");
     const m = args[Math.floor(Math.random() * args.length)];
     const e = new MessageEmbed()
       .setTitle("Randomizinator 2000")
