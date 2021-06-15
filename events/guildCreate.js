3d3dac411fdf0f84aa80e59debc99878e7e354f0
nahan <nathan.demichele@gmail.com>
2021-06-15 00:16:55 -0500

    test

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,13 +1,17 @@
+const { MessageEmbed } = require("discord.js");
 const GuildConfig = require("../database/GuildConfig")
 
 module.exports={
     name: "guildCreate",
     run: async (guild, client) => {
+      const data = await GuildConfig.findOne({ guildId: guild.id })
+      if(!data) {
         await GuildConfig.create({
-            guildName: guild.name,
-            guildId: guild.id,
-          });
-          console.log(`Shiba has joined ${guild.name}. Saved to base`);
+          guildName: guild.name,
+          guildId: guild.id,
+        });
+        console.log(`Shiba has joined ${guild.name}. Saved to base`);
+      }
     }
   }
   
