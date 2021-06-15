5f314f0a377b6e9dad55ffa151b036358b810742
{Matthew} <68120990+MatthewDisc@users.noreply.github.com>
2021-06-15 19:58:49 +0000

    Epic buttons.

--- commands/Info/Botinfo.js
+++ commands/Info/Botinfo.js
@@ -51,8 +51,23 @@ module.exports = {
       .setColor("RANDOM")
       .setFooter(message.author.tag, message.author.displayAvatarURL())
       .setTimestamp();
+
+      
+    const buttons = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+          .setLabel('Invite')
+          .setURL('https://discord.com/oauth2/authorize?client_id=838254815225970739&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=bot%20applications.commands')
+          .setStyle('LINK'),
+				new MessageButton()
+          .setLabel('Support')
+          .setURL('https://discord.gg/kxt4GsrEE6')
+          .setStyle('LINK'),
+			);
+
     message.reply({
       embed: embed,
+      components: [buttons],
       allowedMentions: { repliedUser: false },
     });
   },
