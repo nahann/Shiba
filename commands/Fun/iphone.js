37a041a3897489a7c667e637c0fbd8033a73ba59
nahan <nathan.demichele@gmail.com>
2021-06-15 02:19:41 -0500

    fixed img cmds

--- commands/Fun/iphone.js
+++ commands/Fun/iphone.js
@@ -5,7 +5,8 @@ module.exports = {
   name: "iphone",
   description: "make your avatar a iphone!",
   run: async (client, message, args) => {
-    const avatar = message.mentions.users.first().displayAvatarURL({ dynamic: false }) || message.author.displayAvatarURL({ dynamic: false }) 
+    const user = message.mentions.users.first() || message.author;
+    const avatar = user.displayAvatarURL()
     try {
       const { data } = await get(
         `https://nekobot.xyz/api/imagegen?type=iphonex&url=${avatar}`
--- commands/Fun/magik.js
+++ commands/Fun/magik.js
@@ -5,7 +5,8 @@ module.exports = {
   name: "magik",
   description: "very distortion",
   run: async (client, message, args) => {
-    const avatar = message.mentions.users.first().displayAvatarURL({ dynamic: false }) || message.author.displayAvatarURL({ dynamic: false }) 
+    const user = message.mentions.users.first() || message.author;
+    const avatar = user.displayAvatarURL()
     try {
       const { data } = await get(
         `https://nekobot.xyz/api/imagegen?type=magik&image=${avatar}&intensity=10`
