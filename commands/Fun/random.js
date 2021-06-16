e0ed668d42e01b72045bb4a698347d35896870dd
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-16 16:18:07 +0530

    Update random.js

--- commands/Fun/random.js
+++ commands/Fun/random.js
@@ -5,11 +5,11 @@ module.exports = {
   aliases: ["randomword", "rw"],
   run: async(client, message, args) => {
     if (!args.length) return message.reply("you need some text there!");
-    const m = args[Math.floor(Math.random() * args.length)];
+    const m = args.length > 1 ? args[Math.floor(Math.random() * args.length)] : args[0].split("")[Math.floor(Math.random * args[0].split("").length)]
     const e = new MessageEmbed()
       .setTitle("Randomizinator 2000")
       .setDescription(m)
-      .setFooter("quak");
-    message.channel.send({ embed: e });
+      .setColor("RANDOM")
+    message.channel.send({ embeds: [e] });
   },
 };
