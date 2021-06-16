86f6c5d3c1551135fc03ffad547d6bca10bc28cf
Mahir Molai <mahir@molai.dev>
2021-06-16 20:51:47 +0500

    Update contributors.js

--- commands/Info/contributors.js
+++ commands/Info/contributors.js
@@ -1,29 +1,31 @@
 const Discord = require("discord.js");
 const fetch = require("node-fetch");
+const config = require("../../config.json");
+
 module.exports = {
   name: "contributors",
   description: "List the contributors for Shiba",
   run: async(client, message, args) => {
-    const apiToken = await fetch('https://jsonblob.com/api/jsonBlob/f7a275d2-b119-11eb-b1f1-09924f3a0c66')).json()).apiKey;
     
-    const maintainersJson = await fetch('/repos/nahann/Shiba/contributors', {
-        method: 'GET',
+    var contributors = await (await fetch("https://api.github.com/repos/nahann/Shiba/contributors", {
+        method: "GET",
         headers: {
-          'Authorization': `token ${apiToken}`
+          "Authorization": `token ${config.apiToken}`
         }
-      }).json();
+      })).json();
     
-      const maintainers = [];
-    
-      const push = `{name: ${maintainersJson.login}, value: \`[Click Me To Visit Their GitHub](${maintainersJson.html_url})\`, inline: true}`;
-    
-      await maintainers.push(push);
-    
+      contributors = contributors.map(contributor => new Object({
+        name: contributor.login,
+        value: `[Click Me To Visit Their GitHub](${contributor.html_url})`,
+        inline: true,
+      }));
+
       const embed = new Discord.MessageEmbed()
-      .setTitle("All The Maintainers That Are Working On Shiba!")
-      .setDescription("Be sure to drop a star on the repo, [here!](https://github.com/nahann/Shiba)")
-      .addFields(maintainers)
-      .setFooter("Shiba - This command was made by `molai.dev#9999`!");
+      .setTitle("All The Contributors That Are Helping Make Shiba Awesome!")
+      .setDescription("Be Sure To Drop A Star On The Repo, [Here!](https://github.com/nahann/Shiba)")
+      .addFields(contributors)
+      .setFooter(`Shiba - This Command Was Made By ${client.users.cache.get('763767239018938368').tag}!`);
     
-      message.reply({ embed: embed });
+      message.reply(embed);
   }
+};
