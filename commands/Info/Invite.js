adbc801f5c496b3b9f6ab0a5ac8b537ceabf76ab
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-16 15:57:07 +0530

    Update Invite.js

--- commands/Info/Invite.js
+++ commands/Info/Invite.js
@@ -8,6 +8,6 @@ module.exports = {
      * @param {String[]} args 
      */
     run: async(client, message, args) => {
-        message.reply({ embed: client.embed({ description: `Click **[here](https://discord.com/api/oauth2/authorize?client_id=838254815225970739&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=bot%20applications.commands)** for your link to invite Shiba.`}, message)})
+        message.reply({ embeds: [client.embed({ description: `Click **[here](https://discord.com/api/oauth2/authorize?client_id=838254815225970739&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fdiscord%2Fredirect&scope=bot%20applications.commands)** for your link to invite Shiba.`}, message)]})
     }
 }
