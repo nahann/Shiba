70bf153fe3f2f7a8a7606c0f4c2191d620467b2c
nahan <72932485+nahann@users.noreply.github.com>
2021-06-16 14:49:48 -0500

    Delete src directory

--- src/events/interaction.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "interaction",
-  run: async (command, client) => {
-    if (!command.isCommand()) return;
-    client.slashes.get(command.commandName)?.run(client, command);
-  },
-};
