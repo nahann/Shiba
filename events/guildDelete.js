17dcea76a286bae0e2e50f92cf756cfc16889acf
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-16 08:42:02 +0530

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,6 @@
 const GuildConfig = require("../database/GuildConfig")
 const WelcomeConfig = require('../database/Welcome')
-
+const { MessageEmbed } = require("discord.js")
 module.exports={
     name: "guildDelete",
     run: async (guild, client) => {
@@ -8,6 +8,15 @@ module.exports={
         console.log(`Shiba has left ${guild.name}. Deleted from base`);
         await WelcomeConfig.findOneAndDelete({ guildId: guild.id });
         console.log(`Welcome config deleted from base.`);
+        const channel = client.guilds.cache.find(g => g.name.includes("Shiba Support")).channels.cache.get("854479020028723210"); 
+        channel.send({embed: new MessageEmbed()
+                      .setAuthor(`We lost a server.`, guild.iconURL())
+                      .addField("Name",guild.name,true)
+                      .addField("Membercount",guild.memberCount.toString(),true)
+                      .setThumbnail(guild.iconURL())
+                      .setColor("RANDOM")
+                      .setFooter(`${client.user.tag}`, client.user.displayAvatarURL())
+                      .setTimestamp()})   
     }
   }
   
