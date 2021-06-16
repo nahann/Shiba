014c1b04b89cf629035526854280eb344752cb31
nahan <72932485+nahann@users.noreply.github.com>
2021-06-16 05:39:23 -0500

    Update Rob.js

--- commands/Economy/Rob.js
+++ commands/Economy/Rob.js
@@ -15,18 +15,18 @@ module.exports = {
     run: async(client, message, args) => {
         const schema = await client.db.load("userEcos")
         const user = message.mentions.users.first();
-        if(message.author.id === user.id) return message.reply({ embed: client.embed({ description: `You cannot rob yourself.`}, message)})
+        if(message.author.id === user.id) return message.reply({ embeds: [client.embed({ description: `You cannot rob yourself.`}, message)]})
         const authorData = await schema.findOne({ userId: message.author.id }) || await schema.create({ userId: message.author.id })
         const userData = await schema.findOne({ userId: user.id }) || await schema.create({ userId: user.id })
         const userToken = userData.get('walletShibaToken')
         const authorPassive = authorData.get('Passive')
         const userPassive = userData.get('Passive')
-        if(userToken < 500)return message.reply({ embed: client.embed({ description: `The person you are trying to rob has less than 500 Shiba Token.`}, message)})
-        if(authorPassive === true) return message.reply({ embed: client.embed({ description: `You cannot rob someone while you are on passive mode.`}, message)})
-        if(userPassive === true) return message.reply({ embed: client.embed({ description: `The user you are trying to rob is on passive mode.`}, message)})
+        if(userToken < 500)return message.reply({ embeds: [client.embed({ description: `The person you are trying to rob has less than 500 Shiba Token.`}, message)]})
+        if(authorPassive === true) return message.reply({ embeds: [client.embed({ description: `You cannot rob someone while you are on passive mode.`}, message)]})
+        if(userPassive === true) return message.reply({ embeds: [client.embed({ description: `The user you are trying to rob is on passive mode.`}, message)]})
         const token = Math.floor(Math.random() * 350);
         schema.increment({ userId: message.author.id }, "walletShibaToken", token)
         schema.decrement({ userId: user.id }, "walletShibaToken", token)
-        return message.reply({ embed: client.embed({ description: `${message.author.username} stole ${token} from ${user.username}`}, message)})
+        return message.reply({ embeds: [client.embed({ description: `${message.author.username} stole ${token} from ${user.username}`}, message)]})
     }
 }
