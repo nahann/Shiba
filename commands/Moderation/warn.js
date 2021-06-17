4e2ce73e03f7562591f10f6425090671d13bcbee
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-17 09:58:51 +0530

    Update warn.js

--- commands/Moderation/warn.js
+++ commands/Moderation/warn.js
@@ -7,10 +7,14 @@ module.exports={
      const schema = await client.db.load("warns")
      if(!args.length) return message.reply({embeds: [client.embed({description: "You need to provide someone to warn!"},message)]})
      const shift = args.shift()
+     const reason = args.join(" ") || "No reason provided"
      let user = message.mentions.users.first() || message.guild.members.cache.find(m => m.user.tag.includes(shift))?.user || await message.guild.members.fetch(shift) 
      if(!user) return message.reply("That user does not exist!")
      const docs = await schema.findOne({ user: user.id }) || await schema.create({ user: user.id, warns: []})
-     schema.push({})
+     schema.push({user: user.id},"roles",{id: (docs.roles.length + 1),reason,time: new Date(Date.now())}).then(()=>{
+         user.send(`You have been warned in **${message.guild.name}** for: *${reason}*`)
+         message.reply({embeds: [client.embed({description: `${user.tag} has been warned for: **${reason}**`},message)]})
+     })
      }catch(e){
         console.error(e)
         message.reply({ content: `An error occured!\n${e}`, code: true})
