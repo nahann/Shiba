c36f5c8ca994766a6ee3e5a1c96658d35ccd8318
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-17 10:27:19 +0530

    Update warns.js

--- commands/Info/warns.js
+++ commands/Info/warns.js
@@ -8,8 +8,8 @@ module.exports={
     const schema = await client.db.load("warns")
     const docs = await schema.findOne({ user: user.id })
     if(!docs || !docs.warns.length) return message.reply("This user has no warns!")
-    const mapped = docs.warns.map(w =>`Warn id: ${w.id}\nReason: \"${w.reason}\"\nDate: ${w.time.toLocaleDateString()}`)
-    return message.reply({embeds: [client.embed({title: `Warns for ${user.tag}`,description: mapped.join("\n\n")},message)]})
+    const mapped = docs.warns.map(w =>`Warn ID: ${w.id}\nReason: \"${w.reason}\"\nDate: ${w.time.toLocaleDateString()}`)
+    return message.reply({embeds: [client.embed({title: `Warns for ${user.tag}`,description: mapped.join("\n")},message)]})
     }catch(e){
       console.error(e)
       message.reply({content: `An error occured! ${e}`,code: true})
