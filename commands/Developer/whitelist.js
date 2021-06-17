060917903feed6d15d4698395002304b75d8c8aa
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-17 23:31:43 +0530

    Create whitelist.js

---
+++ commands/Developer/whitelist.js
@@ -0,0 +1,9 @@
+module.exports={
+  name: "whitelist",
+  ownerOnly: true,
+  run: async(client,message,args) =>{
+    if(isNaN(args[0]) return;
+    await (await client.db.load("blacklist")).delete({user: args[0]})
+    message.reply({ embeds: [client.embed({description: `Removed ${args[0]} from the blacklist`},message)]})
+  }
+}
