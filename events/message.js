01cd29d0a99b1e31b9aadacbddcca5a1c29f6b97
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-17 18:32:23 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -40,6 +40,7 @@ module.exports = {
       if (!CC.findOne({ guildId: message.guild.id, commandname: commandName })){
        const best = [client.commands.map(c => c.name)].filter(c => require("leven")(command.toLowerCase(),c.toLowerCase()) < (c.length * 0.4))
        const d = !best.length ? "" : best.length == 1 ? `Did you mean **${best[0]}**?` : `Did you mean any of these? ${best.slice(0,3).map(val => `**${val}**`).join("\n")}` 
+       return message.reply({embeds: [client.embed({description: "Couldn't find that command!"},message)]})
       }
 
       CC.findOne(
