5e6d78250202057140b64dcffcb7edb0e5d80d58
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-17 23:30:31 +0530

    Create Blacklist.js

---
+++ commands/Developer/Blacklist.js
@@ -0,0 +1,9 @@
+module.exports={
+  name: "blacklist",
+  ownerOnly: true,
+  run: async(client,message,args) =>{
+    if(isNaN(args[0]) return;
+    await (await client.db.load("blacklist")).create({user: args[0]})
+    message.reply({ embeds: [client.embed({description: `Added ${args[0]} to the blacklist`},message)]
+  }
+}
