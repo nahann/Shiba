dfad9eaa91e4ec2eeaa8e56ef3d1be8297fe9b2f
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-21 20:29:53 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,5 +1,8 @@
 const ms = require("ms");
 const UserConfig = require("./../database/UserConfig");
+const { mongouri } = require("../config.json")
+    const Levels = require("discord-xp")
+    Levels.setURL(mongouri)
 
 module.exports = {
   name: "message",
@@ -11,6 +14,12 @@ module.exports = {
     const GuildConfig = require("../database/GuildConfig");
     const CC = require("../database/CustomCommands");
     if (message.author.bot) return;
+    const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
+  const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+  if (hasLeveledUp) {
+    const user = await Levels.fetch(message.author.id, message.guild.id);
+    message.reply(`Congratulations! You have leveled up to **${user.level}**. :tada:`);
+  }
     const data =
       (await GuildConfig.findOne({ guildId: message.guild.id })) ||
       (await GuildConfig.create({
