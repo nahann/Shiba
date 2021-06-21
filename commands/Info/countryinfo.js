7f3823a0f1dc6a91d6c30fba507bb4599793cce1
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-21 10:55:25 +0530

    Update countryinfo.js

--- commands/Info/countryinfo.js
+++ commands/Info/countryinfo.js
@@ -10,7 +10,14 @@ module.exports={
        const fetched = await fetch(url).then(res => res.json())
        if(!fetched.length) return message.reply("That country does not exist!")
        const result = fetched[0]
-       message.reply({embeds: [client.embed({title: `Info for ${result.name}`},message).addField("Top Level Domain(s)",result.topLevelDomain?.join(", ") || "None",true).addField("Capital",result.capital,true).addField("Alternate Names",results.altSpellings?.join(", ") || "None")]})
+       message.reply({embeds: [client.embed({title: `Info for ${result.name}`},message)
+                               .addField("Top Level Domain(s)",result.topLevelDomain?.join(", ") || "None",true)
+                               .addField("Capital",result.capital,true)
+                               .addField("Alternate Names",result.altSpellings?.join(", ") || "None")
+                               .addField("Continent",result.region,true)
+                               .addField("Population",result.population.toString(),true)
+                              ]
+                     })
      }
   }
 }
