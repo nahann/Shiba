f89d4393095ab28292fedf8ade63d37f52162e3e
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-21 20:42:23 +0530

    Update rank.js

--- commands/Leveling/rank.js
+++ commands/Leveling/rank.js
@@ -5,7 +5,6 @@ module.exports={
      const target = message.mentions.users.first() || message.author; // Grab the target.
      const user = await client.Levels.fetch(target.id, message.guild.id); // Selects the target from the database.
      if (!user) return message.reply("This user does not have any levels!")
-     console.log(user)
      message.reply({embeds: [client.embed({title: `${target.tag}'s Level`,description: `Level: ${user.level}\nXP: ${user.xp}`},message)]});
    }
 }  
