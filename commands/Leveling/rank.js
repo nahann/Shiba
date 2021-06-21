8abc8b6f57b1bf332601b03eab1fc92be9e40ea3
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-21 20:36:42 +0530

    Create rank.js

---
+++ commands/Leveling/rank.js
@@ -0,0 +1,11 @@
+module.exports={
+   name: "rank",
+   aliases: ["xp","level"],
+   run: async(client,message,args)=>{
+     const target = message.mentions.users.first() || message.author; // Grab the target.
+     const user = await client.Levels.fetch(target.id, message.guild.id); // Selects the target from the database.
+     if (!user) return message.reply("This user does not have any levels!")
+     console.log(user)
+     message.reply({embeds: [client.embed({title: `${target.tag}'s Level`,description: `${user.level}`},message)]});
+   }
+}  
