d4b87be88b26e09bdb27f5f02b2197ee93534ce9
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-21 10:48:44 +0530

    commit so I accidentally dont refresh and have to retype code again

---
+++ commands/Info/countryinfo.js
@@ -0,0 +1,16 @@
+const { fetch } = require("node-fetch")
+module.exports={
+  name: "countryinfo",
+  description: "Get any country's info",
+  args: true,
+  aliases: ["country"],
+  run: async(client,message,args)=>{
+     const url = `https://restcountries.eu/rest/v2/name/${args[0]}`
+     try{
+       const fetched = await fetch(url).then(res => res.json())
+       if(!fetched.length) return message.reply("That country does not exist!")
+       const result = fetched[0]
+       message.reply({embeds: [client.embed({title: `Info for ${result.name}`},message)]})
+     }
+  }
+}
