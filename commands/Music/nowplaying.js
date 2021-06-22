3fc3f2b46a9b69f1e2653495616937b9262e5a74
{Bubbly} <68120990+BubblyDisc@users.noreply.github.com>
2021-06-22 09:42:47 +0000

    Update nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -1,16 +1,17 @@
 const ms = require("pretty-ms")
-module.exports={
+module.exports = {
   name: "np",
   aliases: ["nowplaying"],
   beta: true,
   description: "Get the currently playing song",
-  run: async(client,message,args)=>{
-   const player = client.music.players.get(message.guild.id)
-   if(!player) return message.reply({embeds: [client.embed({title: "There is no song currently playing!"})]})
-   const { current } = player.queue
-   const s = current.endTime ? `\`\`\`diff\n+ ${ms(Date.now() - (current.endTime - current.duration))}/${ms(current.duration)}\`\`\`` : `\`\`\`Playlists do not currently support this feature.\`\`\``
-   message.reply({
-     embeds: [ client.embed({title: `Currently playing: **${current.title}** by **${current.author}**`,description: `${s}\nRequested by: **${current.requester.tag}**` },message).setURL(current.uri).setThumbnail(current.thumbnail) ]
-   })
+  run: async (client, message, args) => {
+    const player = client.music.players.get(message.guild.id)
+    if (!player) return message.reply({ embeds: [client.embed({ title: "There is no song currently playing!", message })] })
+
+    const { current } = player.queue
+    const s = current.endTime ? `\`\`\`diff\n+ ${ms(Date.now() - (current.endTime - current.duration))}/${ms(current.duration)}\`\`\`` : `\`\`\`Playlists do not currently support this feature.\`\`\``
+    message.reply({
+      embeds: [client.embed({ title: `Currently playing: **${current.title}** by **${current.author}**`, description: `${s}\nRequested by: **${current.requester.tag}**` }, message).setURL(current.uri).setThumbnail(current.thumbnail)]
+    })
   }
 }
