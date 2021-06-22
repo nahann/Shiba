f2c014a8ba2809d6e1a79e1dcf8b5e1b62beb3c4
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 09:02:44 +0530

    jesus christ bubbly thats not how you make an emebd

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -139,7 +139,7 @@ module.exports = {
         if (!player.playing && !player.paused && !player.queue.length)
           player.play();
 
-        return message.reply({ embeds: client.embed({ description: `Added ${res.track.title} to the queue`.setThumbnail(res.track.thumbnail) }, message) })
+        return message.reply({ embeds: client.embed({ description: `Added ${track.title} to the queue`}, message).setThumbnail(track.thumbnail) })
     }
   },
 };
