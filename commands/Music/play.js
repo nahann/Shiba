3bb314dfaa3f421fa5667bb14fc2f6e7f96ef33f
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 13:56:30 +0530

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -75,13 +75,13 @@ module.exports = {
         player.queue.add(res.tracks[0]);
         if (!player.playing && !player.paused && !player.queue.length)
           player.play();
-        return message.reply({ embeds: client.embed({ description: `Added ${res.tracks[0].title} to the queue`.setThumbnail(res.tracks[0].thumbnail) }), message })
+        return message.reply({ embeds: [client.embed({ description: `Added ${res.tracks[0].title} to the queue`},message).setThumbnail(res.tracks[0].thumbnail)] })
 
       case "PLAYLIST_LOADED":
         player.queue.add(res.tracks);
         player.play();
         return message.reply({
-          embeds: { description: `Added ${res.tracks[0].title} to the queue`.setThumbnail(res.tracks[0].thumbnail) },
+          embeds: [client.embed({ description: `Added ${res.tracks[0].title} to the queue`},message).setThumbnail(res.tracks[0].thumbnail)],
           message
         })
 
