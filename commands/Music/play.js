49b7286d16c76999a173a9d9ea9f37911facbe05
{Bubbly} <68120990+BubblyDisc@users.noreply.github.com>
2021-06-22 16:13:06 +0000

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -82,8 +82,12 @@ module.exports = {
         player.queue.add(res.tracks);
         const length = res.tracks.length - 3
         const s = res.tracks.length <= 3 ? res.tracks.map(tr => `**${tr.title}**`).join(", ") : `${res.tracks.splice(0,3).map(tr => `**${tr.title}**`).join(", ")} ...and ${length} more`
-        if (player.playing) return message.reply({ embeds: [client.embed({ description: `Added ${s} to the queue`},message).setThumbnail(message.author.displayAvatarURL())], message })
-        if (!player.playing && !player.paused && !player.queue.length) return player.play();
+        
+        player.play();
+        return message.reply({ embeds: [client.embed({ description: `Added ${s} to the queue`},message).setThumbnail(message.author.displayAvatarURL())], message })
+
+
+        return;
 
       case "SEARCH_RESULT":
         let max = 5
