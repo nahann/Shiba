67c2b1a8b3240fd181efc21b7ea9dedcf441742e
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 09:58:18 +0530

    lil testy

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -7,6 +7,7 @@ module.exports={
    const player = client.music.players.get(message.guild.id)
    if(!player) return message.reply({embeds: [client.embed({title: "There is no song currently playing!"})]})
    const { current } = player.queue
+   console.log(player.queue)
    message.reply({
      embeds: [ client.embed({title: `Currently playing: **${current.title}** by **${current.author}**`,description: `${ms(current.endTime - Date.now())}/${current.duration}`}).setURL(current.uri).setThumbnail(current.thumbnail) ]
    })
