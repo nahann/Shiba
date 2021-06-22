9c5134e4e5363cda989ce155e54ae3b3f586f65e
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 14:28:35 +0530

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,7 +2,7 @@ module.exports={
    name: "voiceStateUpdate",
    run: async(old,newc,client)=>{
      const { guild } = old
-     if(!guild.me.voice?.channel?.members.size){
+     if(guild.me.voice?.channel?.members.size == 1){
        const player = client.music.players.get(message.guild.id)
        if(!player) return
        const vchannel = guild.channels.cache.get(player.voiceChannel)
