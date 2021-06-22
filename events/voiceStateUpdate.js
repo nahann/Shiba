2b65e686f1533dc5b12c0835d8f00ada8ab4d20e
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 14:35:54 +0530

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -5,11 +5,10 @@ module.exports={
      if(guild.me.voice?.channel?.members.size == 1){
        const player = client.music.players.get(guild.id)
        if(!player) return
-       const vchannel = guild.channels.cache.get(player.voiceChannel)
        const channel = guild.channels.cache.get(player.textChannel)
        console.log(vchannel.type)
        channel.send("Everyone left, so I did too!")
-       await vchannel.leave()
+       await player.destroy()
      }
    }
 }
