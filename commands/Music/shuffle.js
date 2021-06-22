4edc0e4e9efcc6049e8e3c4d733ded137cad0d92
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 21:56:04 +0530

    Create shuffle.js

---
+++ commands/Music/shuffle.js
@@ -0,0 +1,11 @@
+module.exports={
+  name: "shuffle",
+  description: "Shuffle your quuee, queeueeu, however the fuck you spell it",
+  run: async(client,message,args)=>{
+    if(!message.member.voice.channel) return;
+    const player = client.music.players.get(message.guild.id)
+    if(!player) return;
+    await player.shuffle()
+    message.reply({embeds: [client.embed({title: "Shuffled queeeueueurueurrueueueue."},message)]})
+  }
+}
