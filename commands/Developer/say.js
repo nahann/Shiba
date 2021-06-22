9f446c96760b67fc84999194967e76f94d28b02c
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 23:53:38 +0530

    Update say.js

--- commands/Developer/say.js
+++ commands/Developer/say.js
@@ -3,7 +3,7 @@ module.exports={
   ownerOnly: true,
   args: true,
   run: async(client,message,args)=>{
-    await message.delete()
+    if(message.guild.me.permissions.has("MANAGE_MESSAGES")) await message.delete()
     const msg = args.join(" ").split(" --reply=")
     const msg1 = msg[0]
     const obj = msg[1] && !isNaN(msg[1]) ? {content: msg1,reply: { messageReference: msg[1] }} : { content: msg1 }
