6ce0766b84a2fd6386bb90815990b78030532632
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 14:57:33 +0530

    Update nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -8,8 +8,9 @@ module.exports={
    const player = client.music.players.get(message.guild.id)
    if(!player) return message.reply({embeds: [client.embed({title: "There is no song currently playing!"})]})
    const { current } = player.queue
+   const s = current.endTime ? `\`\`\`diff\n+ ${ms(Date.now() - (current.endTime - current.duration))}/${ms(current.duration)}\`\`\`` : `\`\`\`Playlists do not currently support this feature.\`\`\``
    message.reply({
-     embeds: [ client.embed({title: `Currently playing: **${current.title}** by **${current.author}**`,description: `\`\`\`diff\n+ ${ms(Date.now() - (current.endTime - current.duration))}/${ms(current.duration)}\`\`\`\nRequested by: **${current.requester.tag}**`},message).setURL(current.uri).setThumbnail(current.thumbnail) ]
+     embeds: [ client.embed({title: `Currently playing: **${current.title}** by **${current.author}**`,description: \nRequested by: **${current.requester.tag}**`},message).setURL(current.uri).setThumbnail(current.thumbnail) ]
    })
   }
 }
