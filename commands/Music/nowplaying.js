125bee246b73a7bbfba6c39865b9b82bec7dbbc9
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 10:01:18 +0530

    im very dumb

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -6,10 +6,9 @@ module.exports={
   run: async(client,message,args)=>{
    const player = client.music.players.get(message.guild.id)
    if(!player) return message.reply({embeds: [client.embed({title: "There is no song currently playing!"})]})
-   const current = player.queue.current
-   console.log(current)
+   const { current } = player.queue
    message.reply({
-     embeds: [ client.embed({title: `Currently playing: **${current.title}** by **${current.author}**`,description: `${ms(current.endTime - Date.now())}/${current.duration}`}).setURL(current.uri).setThumbnail(current.thumbnail) ]
+     embeds: [ client.embed({title: `Currently playing: **${current.title}** by **${current.author}**`,description: `${ms(current.endTime - Date.now())}/${current.duration}`},message).setURL(current.uri).setThumbnail(current.thumbnail) ]
    })
   }
 }
