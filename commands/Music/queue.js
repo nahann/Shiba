d493b8e6339418dc41f44a6736821169cde1f4da
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 10:34:53 +0530

    Create queue.js

---
+++ commands/Music/queue.js
@@ -0,0 +1,17 @@
+module.exports={
+  name: "queue",
+  beta: true,
+  run: async(client,message,args)=>{
+   const player = client.music.players.get(message.guild.id)
+   if(!player) return message.reply({embeds: [client.embed({title: "There is no song playing!"},message)]})
+   const { queue } = player;
+   let s = `Currently playing: **${queue.current.title}** by **${queue.current.author}**` 
+   if(queue.length){
+    let i = 0
+    queue.forEach(track =>{
+    i++
+    s += `\n${i}: **${track.title}** by **${track.author}**`  
+    })
+   }
+  }
+}
