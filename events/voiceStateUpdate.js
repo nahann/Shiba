dc270a171ffd944ad55d3dabe4ef38619f72a9e4
{Bubbly} <68120990+BubblyDisc@users.noreply.github.com>
2021-06-22 09:14:47 +0000

    Epic embed

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,11 +2,18 @@ module.exports={
    name: "voiceStateUpdate",
    run: async(old,newc,client)=>{
      const { guild } = old
+
      if(guild.me.voice?.channel?.members.size == 1){
        const player = client.music.players.get(guild.id)
-       if(!player) return
+       if(!player) return;
+
        const channel = guild.channels.cache.get(player.textChannel)
-       channel.send("Everyone left, so I did too!")
+
+       let texts = ['Everyone left me so i left crying', 'Everyone left me so i left **poping a chocky milk**']
+       let Picker = Math.floor(Math.random() * texts.length)
+
+       channel.send({ embeds: [client.embed({ description: texts[Picker] }, message)] })
+
        await player.destroy()
      }
    }
