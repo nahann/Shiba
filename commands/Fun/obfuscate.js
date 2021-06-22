cabcede1d9b50411e8ecab8f3aed1ae2025701e1
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-22 23:09:22 +0530

    new fucked up obfuscated command

---
+++ commands/Fun/obfuscate.js
@@ -0,0 +1,46 @@
+const _0x38d8 = [
+'1IMoFyu',
+'78955MaBDUQ',
+'9497FEJvXV',
+'297756SYXOMd',
+'526093vxcBzE',
+'obfuscate',
+'4AbVEkk',
+'getObfuscatedCode',
+'Obfuscate\x20your\x20javascript\x20code',
+'join',
+'6719njnFZb',
+'You\x20need\x20to\x20enter\x20some\x20code\x20to\x20obfuscate!',
+'168823uGnAHL',
+'javascript-obfuscator',
+'176593eJEJeq',
+'length'
+]
+const _0x2249ec = _0x217b
+function _0x217b(_0xacc08f,_0x5054c2){
+  _0xacc08f=_0xacc08f-0xa6
+  let _0x38d896=_0x38d8[_0xacc08f]
+  return _0x38d896
+}(function(_0x90c40c,_0x5af470){
+  const _0x193f59 = _0x217b
+  while(!![]){
+try{
+  const _0x39b3fa = parseInt(_0x193f59(0xb5)) +- parseInt(_0x193f59(0xab)) +- parseInt(_0x193f59(0xaf)) *- parseInt(_0x193f59(0xb1)) + parseInt(_0x193f59(0xb2)) +- parseInt(_0x193f59(0xb4)) + parseInt(_0x193f59(0xb3)) *- parseInt(_0x193f59(0xa7)) +- parseInt(_0x193f59(0xad))
+  if(_0x39b3fa===_0x5af470) break
+  else _0x90c40c['push'](_0x90c40c['shift']())
+   }catch(_0x36f3c8){
+  _0x90c40c['push'](_0x90c40c['shift']())
+   }
+ }
+}(_0x38d8,0x42013))
+const obfus = require(_0x2249ec(0xae))
+module['exports'] = {
+  'name': _0x2249ec(0xa6),
+  'description': _0x2249ec(0xa9),
+  'run': async(_0x5443b7,_0x25cd69,_0x32c5e4)=>{
+   const _0x1f6605 = _0x2249ec
+   if(!_0x32c5e4[_0x1f6605(0xb0)]) return _0x25cd69['reply'](_0x1f6605(0xac))
+   const _0x5c61ad = obfus['obfuscate'](_0x32c5e4[_0x1f6605(0xaa)]('\x20'))[_0x1f6605(0xa8)]()
+   _0x25cd69['reply']({'content':_0x5c61ad,'split':!![],'code':!![]});
+  }
+};
