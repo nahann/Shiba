db101e2d2e733705a3acd83a439b9f391ee7187d
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-26 20:45:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,10 +77,10 @@ client.loadCommands = function () {
   const commandFolders = fs.readdirSync(`${__dirname}/commands`);
   for (const folder of commandFolders) {
     const commandFiles = fs
-      .readdirSync(`./commands/${folder}`)
+      .readdirSync(`${__dirname}/commands/${folder}`)
       .filter((file) => file.endsWith(".js"));
     for (const file of commandFiles) {
-      const command = require(`./commands/${folder}/${file}`);
+      const command = require(`${__dirname}/commands/${folder}/${file}`);
       if (!command.category) command.category = folder;
       client.commands.set(command.name, command);
     }
