7003fab2a8de0f238294e6f2a70c684ef7968a60
owencubes <63976821+owencubes@users.noreply.github.com>
2021-06-27 19:13:13 -0400

    Steal Emojis command

---
+++ commands/Moderation/Steal.js
@@ -0,0 +1,29 @@
+const { Client, Message, Util } = require("discord.js");
+module.exports = {
+  name: "steal-emoji",
+  userPermissions: "MANAGE_EMOJIS",
+  args: true,
+  guildOnly: true,
+  category: `Moderation`,
+  description: `Bans a member from the guild.`,
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    emojisarr = [];
+    for (const rawemoji of args) {
+      const parsed = Util.parseEmoji(rawemoji);
+      if (parsed.id) {
+        const extention = parsed.animated ? ".gif" : ".png";
+        const url = `https://cdn.discordapp.com/emojis/${
+          parsed.id + extention
+        }`;
+        const emoji = await message.guild.emojis.create(url, parsed.name);
+        emojisarr.push(emoji);
+      }
+    }
+    message.channel.send(`added emoji(s): ${emojisarr.join(", ")}`);
+  }, 
+};
--- events/ready.js
+++ events/ready.js
@@ -5,8 +5,10 @@ module.exports = {
   name: "ready",
   once: true,
   run: async function (client) {
-   await client.user.setActivity(`${client.guilds.cache.size} servers`,{type: "WATCHING"})
-   require("child_process").exec("git push");
+    await client.user.setActivity(`${client.guilds.cache.size} servers`, {
+      type: "WATCHING",
+    });
+    require("child_process").exec("git push");
     client.music.init(client.user.id);
     console.log(`${client.user.username} is now online.`);
     client.slashes = new Discord.Collection();
@@ -20,7 +22,7 @@ module.exports = {
 
         if (!cmd.name || !cmd.description || !cmd.run) return;
         client.application?.commands.create(cmd);
-        client.guilds.cache.get("849131192275566613").commands.create(cmd);
+        client.guilds.cache.get("783706077229351022").commands.create(cmd);
         client.slashes.set(cmd.name, cmd);
       });
     }
