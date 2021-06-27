0e79faac6595e224c1d1b3fdbe53e584ec72f29e
owencubes <63976821+owencubes@users.noreply.github.com>
2021-06-27 19:14:34 -0400

    Steal Emojis command

--- events/ready.js
+++ events/ready.js
@@ -5,10 +5,8 @@ module.exports = {
   name: "ready",
   once: true,
   run: async function (client) {
-    await client.user.setActivity(`${client.guilds.cache.size} servers`, {
-      type: "WATCHING",
-    });
-    require("child_process").exec("git push");
+   await client.user.setActivity(`${client.guilds.cache.size} servers`,{type: "WATCHING"})
+   require("child_process").exec("git push");
     client.music.init(client.user.id);
     console.log(`${client.user.username} is now online.`);
     client.slashes = new Discord.Collection();
@@ -22,7 +20,7 @@ module.exports = {
 
         if (!cmd.name || !cmd.description || !cmd.run) return;
         client.application?.commands.create(cmd);
-        client.guilds.cache.get("783706077229351022").commands.create(cmd);
+        client.guilds.cache.get("849131192275566613").commands.create(cmd);
         client.slashes.set(cmd.name, cmd);
       });
     }
