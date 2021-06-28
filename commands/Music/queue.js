164a5eff9eb9785de0b55068e83576d9b5b3bbfa
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-28 14:17:47 +0530

    Update queue.js

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -13,6 +13,6 @@ module.exports={
     s.push(`${i}: **${track.title}** by **${track.author}**`)
     })
    }
-    message.reply({embeds: [client.embed({title: `Queue`,description: client.trimArray(s,10).join("\n")},message)]})
+    message.reply({embeds: [client.embed({title: `Queue`,description: client.trimArray(s,10)},message)]})
   }
 }
