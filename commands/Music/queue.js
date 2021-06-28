98028743ad670bd389803be6e8bb31e26d9b167b
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-28 14:14:38 +0530

    Update queue.js

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -5,14 +5,14 @@ module.exports={
    const player = client.music.players.get(message.guild.id)
    if(!player) return message.reply({embeds: [client.embed({title: "There is no song playing!"},message)]})
    const { queue } = player;
-   let s = `Currently playing: **${queue.current.title}** by **${queue.current.author}**` 
+   let s = [`Currently playing: **${queue.current.title}** by **${queue.current.author}**`]
    if(queue.length){
     let i = 0
     queue.forEach(track =>{
     i++
-    s += `\n${i}: **${track.title}** by **${track.author}**`  
+    s.push(`${i}: **${track.title}** by **${track.author}**`)
     })
    }
-    message.reply({embeds: [client.embed({title: `Queue`,description: s},message)]})
+    message.reply({embeds: [client.embed({title: `Queue`,description: client.trimArray(s,10).join("\n")},message)]})
   }
 }
