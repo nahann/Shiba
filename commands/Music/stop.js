a70a29a6e0e2f0d37359a1cfb9b6676e821ed2d8
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-29 19:36:55 +0530

    Update stop.js

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -23,7 +23,7 @@ module.exports = {
 
     player.destroy();
 
-    let texts = ['You stopped a song that i liked for once.', 'You just wanted to get rid of me **pops a choccy milk**', 'I stopped the song, I didn't like it anyway.']
+    let texts = ['You stopped a song that i liked for once.', 'You just wanted to get rid of me **pops a choccy milk**', 'I stopped the song, I didn\'t like it anyway.']
     let Picker = Math.floor(Math.random() * texts.length)
 
     message.reply({ embeds: [client.embed({ description: texts[Picker] }, message)] })
