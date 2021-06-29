5975ba5380b04427c7b8ffa2cdef54e8aa1237c7
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-29 17:04:17 +0530

    Update error.js

--- events/error.js
+++ events/error.js
@@ -1,9 +1,9 @@
 module.exports={
   name: "error",
   run: async(error,client)=>{
+    console.error(error)
     if(!client.cmdmsg) return
     const message = client.cmdmsg
-    console.error(error)
     message.reply({ embeds: [client.embed({title: "Error!",description: `\`\`\`\n${error}\`\`\``},message).setColor("RED")] })
   }
 }
