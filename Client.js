c9da378b63ee107fbd1171cf7f62d50df8ef1c03
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-29 19:17:36 +0530

    Update Client.js

--- Client.js
+++ Client.js
@@ -1,4 +1,4 @@
-const { Client, MessageEmbed } = require("discord.js")
+const { Client, MessageEmbed, Collection} = require("discord.js")
 const { Database } = require("zapmongo")
 class ShibaClient extends Client{
   constructor(options){
@@ -42,5 +42,37 @@ class ShibaClient extends Client{
       },
     ],
    });
+  this.cooldowns = new Collection();
+  this.commands = new Collection();
   }
+  trimArray(ar,num,join = ", "){
+   const l = ar.length - num
+   return ar.length > num ? `${ar.splice(0,num).join(join)} ${join == "\n" ? "\n" : ""}...and ${l} more` : ar.join(", ")
+  } 
+  loadCommands() {
+  const commandFolders = fs.readdirSync(`${__dirname}/commands`);
+  for (const folder of commandFolders) {
+    const commandFiles = fs
+      .readdirSync(`${__dirname}/commands/${folder}`)
+      .filter((file) => file.endsWith(".js"));
+    for (const file of commandFiles) {
+      const command = require(`${__dirname}/commands/${folder}/${file}`);
+      if (!command.category) command.category = folder;
+      this.commands.set(command.name, command);
+    }
+  }
+  loadEvents(){
+  const eventFiles = fs
+    .readdirSync(__dirname + "/events")
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of eventFiles) {
+    const event = require(`${__dirname}/events/${file}`);
+    if (event.once) {
+      this.once(event.name, (...args) => event.run(...args, client));
+    } else {
+      this.on(event.name, (...args) => event.run(...args, client));
+    }
+  }
+};
 }
