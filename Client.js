5f36b675a38d479eb9ecb30b3b9455418c60a203
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-29 19:11:24 +0530

    Update Client.js

--- Client.js
+++ Client.js
@@ -1,4 +1,46 @@
 const { Client, MessageEmbed } = require("discord.js")
+const { Database } = require("zapmongo")
 class ShibaClient extends Client{
-  constructor(){}
+  constructor(options){
+    super(options)
+    this.db = new Database({
+  mongoURI: config.mongouri,
+  schemas: [
+    {
+      name: "userEcos",
+      data: {
+        userId: String,
+        walletShibaToken: {
+          type: Number,
+          default: 500,
+        },
+        bankShibaToken: {
+          type: Number,
+          default: 0,
+        },
+        Passive: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    },
+    {
+      name: "warns",
+      data: {
+        warns: Array,
+        user: String,
+        guild: String,
+      },
+    },
+    {
+      name: "blacklist",
+      data: { user: String },
+    },
+      {
+      name: "levelguilds",
+      data: { guild: String, onoff: { type: Boolean, default: false } },
+      },
+    ],
+   });
+  }
 }
