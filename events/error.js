278dd57ffc86c01cca7bec91d23365411875965c
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-29 17:03:42 +0530

    Update error.js

--- events/error.js
+++ events/error.js
@@ -1,6 +1,6 @@
 module.exports={
   name: "error",
-  run: async(client,error)=>{
+  run: async(error,client)=>{
     if(!client.cmdmsg) return
     const message = client.cmdmsg
     console.error(error)
