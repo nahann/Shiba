af6a9bb5b860aa65377aac5c1b24abb08d0bce78
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-06-29 19:27:02 +0530

    teaty

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Manager } = require("erela.js");
 const Spotify = require("erela.js-spotify");
 const ms1 = require("pretty-ms");
-
+const Client = require("./Client")
 const Discord = require("discord.js"),
-  client = new Discord.Client({
+  client = new Client({
     ws: { properties: { $browser: "Discord iOS" } },
     intents: require("discord.js").Intents.ALL,
     allowedMentions: {
@@ -27,95 +27,8 @@ mongoose
   .then(() => {
     console.log("Connected to database.");
   });
-
-client.db = new Database({
-  mongoURI: config.mongouri,
-  schemas: [
-    {
-      name: "userEcos",
-      data: {
-        userId: String,
-        walletShibaToken: {
-          type: Number,
-          default: 500,
-        },
-        bankShibaToken: {
-          type: Number,
-          default: 0,
-        },
-        Passive: {
-          type: Boolean,
-          default: false,
-        },
-      },
-    },
-    {
-      name: "warns",
-      data: {
-        warns: Array,
-        user: String,
-        guild: String,
-      },
-    },
-    {
-      name: "blacklist",
-      data: { user: String },
-    },
-    {
-      name: "levelguilds",
-      data: { guild: String, onoff: { type: Boolean, default: false } },
-    },
-  ],
-});
-client.trimArray = function(ar,num,join = ", "){
-   const l = ar.length - num
-   return ar.length > num ? `${ar.splice(0,num).join(join)} ${join == "\n" ? "\n" : ""}...and ${l} more` : ar.join(", ")
-}
-client.cooldowns = new Discord.Collection();
-client.commands = new Discord.Collection();
-client.loadCommands = function () {
-  const commandFolders = fs.readdirSync(`${__dirname}/commands`);
-  for (const folder of commandFolders) {
-    const commandFiles = fs
-      .readdirSync(`${__dirname}/commands/${folder}`)
-      .filter((file) => file.endsWith(".js"));
-    for (const file of commandFiles) {
-      const command = require(`${__dirname}/commands/${folder}/${file}`);
-      if (!command.category) command.category = folder;
-      client.commands.set(command.name, command);
-    }
-  }
-};
-client.loadEvents = function () {
-  const eventFiles = fs
-    .readdirSync(__dirname + "/events")
-    .filter((file) => file.endsWith(".js"));
-
-  for (const file of eventFiles) {
-    const event = require(`${__dirname}/events/${file}`);
-    if (event.once) {
-      client.once(event.name, (...args) => event.run(...args, client));
-    } else {
-      client.on(event.name, (...args) => event.run(...args, client));
-    }
-  }
-};
 client.loadEvents();
 client.loadCommands();
-client.embed = (options, message) => {
-  const emb = new Discord.MessageEmbed({ ...options, color: "RANDOM" })
-    .setFooter(
-      `${message.author.tag}`,
-      message.author.displayAvatarURL({ dynamic: true, format: "png" })
-    )
-    .setTimestamp();
-  if (options.colors && Array.isArray(options.colors))
-    emb.setColor(
-      options.colors[Math.floor(Math.random() * options.colors.length)]
-    );
-
-  return emb;
-};
 client.music = new Manager({
   nodes: [
     {
