acea6ae3a4e40675635e3eda332279d432499f10
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-02 10:22:08 +0530

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -114,7 +114,6 @@ module.exports = {
           await msg.awaitMessageComponentInteraction(filter, { max: 1, time: 100000, errors: ['time'] })
             .then(async(collected)=>{
               const interaction = await collected
-              interaction.defer({ ephemeral: true})
               if (interaction.customID === 'b1') {
                 number = '0'
               } else if (interaction.customID === 'b2') {
@@ -126,7 +125,8 @@ module.exports = {
               } else if (interaction.customID === 'b5') {
                 number = '4'
               }
-             interaction.editReply("Successful!")
+             msg.edit({embeds: msg.embeds, components: []})
+             interaction.reply({content: "Successful!",ephemeral: true})
             })
         } catch (e) { console.error(e) }
 
