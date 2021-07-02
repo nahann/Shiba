892256ba08d177ff164db6df00477c78dcb1dab3
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-02 20:57:06 +0530

    Update Eval.js

--- commands/Developer/Eval.js
+++ commands/Developer/Eval.js
@@ -1,4 +1,4 @@
-const { Client, Message, MessageEmbed, Util } = require("discord.js");
+const { Client, Message, MessageEmbed, Util, MessageButton, MessageActionRow } = require("discord.js");
 const { inspect } = require("util");
 module.exports = {
   name: "eval",
@@ -20,15 +20,24 @@ module.exports = {
       .replace(reg, "you thought");
     try {
       const evaled = await eval(result);
-      const split = Util.splitMessage(inspect(evaled, { depth: 0 }))[0];
-      message.reply({
+      const spl = Util.splitMessage(inspect(evaled, { depth: 0 }))
+      const split = spl.shift()
+      const obj = {
         embeds: [
           client.embed(
             { title: "Eval Success!", description: `\`\`\`\n${split}\`\`\`` },
             message
           ),
         ],
-      });
+      }
+      if(spl.length){
+        const newspl = spl.slice(0,4)
+        newspl.map(result =>{
+         obj.embeds.push(client.embed({ description: \`\`\`\n${result}\`\`\`},message)
+        })
+      }
+      message.reply(obj);
+     
     } catch (err) {
       client.emit("error",err)
     }
