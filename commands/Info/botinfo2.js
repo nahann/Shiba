34c2f0df6b6c16edf02234cb2d1fd98dc011fea7
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-02 10:01:45 +0530

    Update botinfo2.js

--- commands/Info/botinfo2.js
+++ commands/Info/botinfo2.js
@@ -7,7 +7,7 @@ module.exports={
      const mempercentage = `${((os.freemem() / os.totalmem()) * 100).toFixed(2)}%`
      const usedmem = `${(((os.totalmem() - os.freemem()) / os.totalmem()) * 100).toFixed(2)}%`
      message.reply({
-      embeds: [client.embed({title: "System Info for Shiba"},message).addField("Platform",os.platform(),true).addField("Free Memory",mempercentage,true).addField("Used Memory",usedmem,true).addField("System Uptime",ms(os.sysUptime() * 1000),true).addField("Process Uptime",ms(os.processUptime() * 1000),true)]
+      embeds: [client.embed({title: "System Info for Shiba"},message).addField("Platform",os.platform(),true).addField("Free Memory",mempercentage,true).addField("Used Memory",usedmem,true).addField("Full Memory",`${os.totalmem().toFixed(2)}MB`,true).addField("System Uptime",ms(os.sysUptime() * 1000),true).addField("Process Uptime",ms(os.processUptime() * 1000),true)]
      })
    }
 }
