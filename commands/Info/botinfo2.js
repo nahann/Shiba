9505c0933781c7909d68fad81ed97b03f65983c9
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-02 09:54:00 +0530

    Update botinfo2.js

--- commands/Info/botinfo2.js
+++ commands/Info/botinfo2.js
@@ -4,8 +4,8 @@ module.exports={
    name: "info2",
    description: "System info and stuff idk",
    run: async(client,message,args)=>{
-     const mempercentage = `${Math.floor((os.freemem() / os.totalmem()) * 100)}%`
-     const usedmem = `${Math.floor(((os.totalmem() - os.freemem()) / os.totalmem()) * 100)}%`
+     const mempercentage = `${((os.freemem() / os.totalmem()) * 100).toFixed(2)}%`
+     const usedmem = `${(((os.totalmem() - os.freemem()) / os.totalmem()) * 100).toFixed(2)}%`
      message.reply({
       embeds: [client.embed({title: "System Info for Shiba"},message).addField("Platform",os.platform(),true).addField("Free Memory",mempercentage,true).addField("Used Memory",usedmem,true).addField("System Uptime",ms(os.sysUptime() * 1000),true).addField("Process Uptime",ms(os.processUptime() * 1000),true)]
      })
