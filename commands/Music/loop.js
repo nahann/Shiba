9a2c686e3ff324f899e57c6a1dab74d349abeb37
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-04 02:24:36 -0700

    queue loop functinality

--- commands/Music/loop.js
+++ commands/Music/loop.js
@@ -1,10 +1,11 @@
 module.exports={
   name: "loop",
   description: "Loop the current track",
-  run: async(client,message)=>{
+  run: async(client,message,args)=>{
+    const method = args[0] == "queue" ? "setQueueRepeat" : "setTrackRepeat"
     const player = client.music.players.get(message.guild.id)
     if(!message.member.voice.channel || message.member.voice.channel.id !== player.voiceChannel) return
-    player.setTrackRepeat(true)
+    player[method](true)
     message.reply({
        embeds: [ client.embed({title: `Looped ${player.queue.current.title}`},message) ]
     })
