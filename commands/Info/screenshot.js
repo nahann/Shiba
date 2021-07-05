3c2b37e27027791a0b0e33154ee0d7b6f8f73668
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-05 08:20:09 -0700

    Update screenshot.js

--- commands/Info/screenshot.js
+++ commands/Info/screenshot.js
@@ -1,10 +1,11 @@
   const Screenshotter = require("discord-screenshot");
+  const ShibaError = require("../../uti/ShibaError")
 module.exports = {
     name: "screenshot",
     args: true,
     run: async(client, message, args) => {
-       if(message.guild.id == "655109296400367618") return client.emit("error", new Error("You can't use this command in your server because salvage gets butthurt"))
-       if(["porn","sex","xhamster","hentai","fuck","gore"].some(y => args[0].toLowerCase().includes(y))) return client.emit("error",new Error("NSFW sites are not allowed"))
+       if(message.guild.id == "655109296400367618") return client.emit("error", new ShibaError("You can't use this command in your server because salvage gets butthurt"))
+       if(["porn","sex","xhamster","hentai","fuck","gore"].some(y => args[0].toLowerCase().includes(y))) return client.emit("error",new ShibaError("NSFW sites are not allowed"))
         message.channel.startTyping()
        const res = await Screenshotter.screenshot(args[0])
        console.log(res)
