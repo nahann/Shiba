ed66b78571c26770d7f5a56e77a4ec11c60ba3ac
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-05 08:03:09 -0700

    Update screenshot.js

--- commands/Info/screenshot.js
+++ commands/Info/screenshot.js
@@ -3,6 +3,7 @@ module.exports = {
     name: "screenshot",
     args: true,
     run: async(client, message, args) => {
+       if(["porn","sex","xhamster","hentai","fuck"].some(y => args[0].includes(y))) return client.emit("error",new Error("NSFW sites are not allowed"))
         message.channel.startTyping()
        const res = await Screenshotter.screenshot(args[0])
        console.log(res)
