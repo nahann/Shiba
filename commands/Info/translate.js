cfa5524e678ddb5cc953f07cd623d97d06796ce5
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2021-07-08 23:19:02 +0200

    Read description if you dont want an error :>
    Install the required libs wsith the command: npm i @iamtraction/google-translate && npm i iso-language-converter

---
+++ commands/Info/translate.js
@@ -0,0 +1,24 @@
+const translate = require("@iamtraction/google-translate")
+const isoConv = require("iso-language-converter")
+module.exports = {
+  name: "translate",
+  description: "Translate a string.",
+  usage: "*translate <String>",
+    run: async(client, message, args) => {
+        if(args.all.length < 1){
+            return message.reply({embeds: [
+              client.embed({title:"Error", description: "Please provide something to translate."}, message)
+            ]
+                                  )
+        }
+        const translated = await translate(args.all, {to: "en"})
+        if(!translated){
+            return message.reply("Error, try again in a bit.")
+        } else {
+            const iso = isoConv(translated.from.language.iso, {from: 1, to: 'label'})
+            return message.reply({
+              embeds: [client.embed({title: "Translation", description:`Input Text:\n${args.all}\n\nTranslated Text:\n${translated.text}\n\nMade with :heart: by Korabi`}, message).setFooter(`Translated From ${iso}`)]})
+        }
+
+    }
+}
