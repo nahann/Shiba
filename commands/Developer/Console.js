a726a6265f0fdf4bf4351551103213f40316ca81
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-11 11:33:45 -0700

    Update Console.js

--- commands/Developer/Console.js
+++ commands/Developer/Console.js
@@ -15,7 +15,7 @@ module.exports = {
     exec(`cd /root/Shiba && ${args.join(" ")}`, (error, stdout) => {
       const response = error || stdout;
       try {
-        message.reply({ content: `.${response}`, code: true,split: true});
+        message.reply({ content: `.${response}`, code: "sh",split: true});
       } catch {
         message.reply({
           embeds: [
