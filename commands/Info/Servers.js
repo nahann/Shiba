30f48ca95243d183baaa928e495c6309984131be
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-15 01:56:11 -0700

    Update Servers.js

--- commands/Info/Servers.js
+++ commands/Info/Servers.js
@@ -6,7 +6,7 @@ module.exports ={
      client.guilds.cache.map(guild => servers.push({ name: guild.name,count: guild.memberCount}))
      servers.sort((a,b)=> a.count - b.count)
      let i = 0
-     const ar = servers.map(server =>{i++; `${i}: **${server.name}** -- ${server.count} members`})
+     const ar = servers.map(server =>{i++; return `${i}: **${server.name}** -- ${server.count} members`})
      const str = client.trimArray(ar,20,"\n")
      console.log(ar)
      message.reply({ embeds: [client.embed({title: `${client.user.username}'s servers`,description: str},message)] })
