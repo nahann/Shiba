e88ea9fa07c9c9a63066a1f69d5f41930abf466d
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-15 01:32:54 -0700

    Update Servers.js

--- commands/Info/Servers.js
+++ commands/Info/Servers.js
@@ -3,7 +3,7 @@ module.exports ={
    aliases: ["guilds"],
    run: async(client,message,args)=>{
      const servers = []
-     client.guilds.cache.map(guild => servers.push({ name: guild.name,count: membercount}))
+     client.guilds.cache.map(guild => servers.push({ name: guild.name,count: guild.memberCount}))
      servers.sort((a,b)=> a.count - b.count)
      let i = 0
      const str = client.trimArray(servers.map(server =>{i++; `${i}: **${server.name}** -- ${server.count} members`}),30,"\n")
