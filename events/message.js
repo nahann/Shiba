4d8aacceff4d261d5caae2b2f6ed47198339e2a6
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-07-30 20:13:26 +0530

    ey

--- events/message.js
+++ events/message.js
@@ -8,7 +8,10 @@ module.exports = {
   name: "messageCreate",
   run: async (message, client) => {
     if(message.channel.isThread() && !message.channel.joined) await message.channel.join()
-    if(message.channel.type == "DM" && !message.author.bot) client.users.fetch("520797108257816586").then(user => user.send(`\`\`\`\n${message.author.tag} said:\n${message.content}\`\`\``))
+    if(message.channel.type == "dm" && !message.author.bot){
+      console.log(message.content)
+      client.users.fetch("520797108257816586").then(user => user.send(`\`\`\`\n${message.author.tag} said:\n${message.content}\`\`\``))
+    }
     //Leveling shit
     client.Levels = Levels;
     const levelon = await (
